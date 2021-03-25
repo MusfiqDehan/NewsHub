8a18c1ccd5691524bad7c644f571dc083f22213b
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2021-03-25 23:58:08 +0600

    Create News class in models.py of news

--- news/models.py
+++ news/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+    headline = models.CharField(max_length=1024)
+    original_link = models.CharField(max_length=2048)
+    newspaper_name = models.CharField(max_length=255)
+    img_link = models.CharField(max_length=2048)

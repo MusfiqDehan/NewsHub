5f4ad06c75e5a88825f0acec2dc7987d55a9af77
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2021-03-26 16:06:22 +0600

    Change list_display of 'News' model and  add columns

--- news/admin.py
+++ news/admin.py
@@ -5,4 +5,8 @@ admin.site.site_header = "NewsHub Admin"
 admin.site.site_title = "NewsHub Admin Area"
 admin.site.index_title = "Welcome to NewsHub Admin"
 
-admin.site.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'headline', 'original_link', 'newspaper_name', 'img_link')
+
+
+admin.site.register(News, NewsAdmin)

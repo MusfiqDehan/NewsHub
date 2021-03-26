831ef3c10241044560623659ad2c2261711a47fd
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2021-03-26 12:18:54 +0600

    Edit the name and title of admin panel

--- news/admin.py
+++ news/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import News
 
+admin.site.site_header = "NewsHub Admin"
+admin.site.site_title = "NewsHub Admin Area"
+admin.site.index_title = "Welcome to NewsHub Admin"
+
 admin.site.register(News)

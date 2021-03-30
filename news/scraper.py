617b4c4057f8300d97c33aee5cd2e2fca7e050ed
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2021-03-31 00:47:55 +0600

    Add aljazeera News in scraper.py

--- news/scraper.py
+++ news/scraper.py
@@ -18,7 +18,10 @@ def scrape(website_url, tag_name, class_name):
 
 bbc_news = scrape("https://www.bbc.com/news", 'h3', 'nw-o-link-split__text')
 nbc_news = scrape("https://www.nbcnews.com/", 'h2', 'alacarte__headline')
+aljazeera = scrape("https://www.aljazeera.com/", 'a', 'fte-featured__title__link')
 
 print(bbc_news)
 print('\n')
 print(nbc_news)
+print('\n')
+print(aljazeera)

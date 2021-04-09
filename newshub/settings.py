c4b7ad0a96c089a4611617605f7c94fff7c63920
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2021-04-09 20:31:53 +0600

    Add default auto field at settings.py to fix error

--- newshub/settings.py
+++ newshub/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,4 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'static'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'

e5772c2f571976aaa366e118150769ba9eebad8e
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2021-05-14 00:02:13 +0600

    Add search functionality, change navbar styles

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect 
+from django.shortcuts import render, redirect
 import json
 from django.http import HttpResponse, JsonResponse
 from django.forms import inlineformset_factory
@@ -14,78 +14,82 @@ from .forms import CreateUserForm
 
 bbc_news = scrape("https://www.bbc.com/news", 'h3', 'nw-o-link-split__text')
 nbc_news = scrape("https://www.nbcnews.com/", 'h2', 'alacarte__headline')
-aljazeera = scrape("https://www.aljazeera.com/", 'a', 'fte-featured__title__link')
-prothom_alo = scrape("https://www.prothomalo.com/collection/latest", 'h2', 'headline')
+aljazeera = scrape("https://www.aljazeera.com/",
+                   'a', 'fte-featured__title__link')
+prothom_alo = scrape(
+    "https://www.prothomalo.com/collection/latest", 'h2', 'headline')
 manob_jomin = scrape("https://mzamin.com/category.php?cid=8", 'a', '')
-kaler_kontho = scrape("https://www.kalerkantho.com/online/country-news/", 'a', 'title hidden-xs')
+kaler_kontho = scrape(
+    "https://www.kalerkantho.com/online/country-news/", 'a', 'title hidden-xs')
 
-zipped_data = zip(bbc_news, nbc_news, aljazeera, prothom_alo, manob_jomin, kaler_kontho)
+zipped_data = zip(bbc_news, nbc_news, aljazeera,
+                  prothom_alo, manob_jomin, kaler_kontho)
 
 
 def registerPage(request):
-	if request.user.is_authenticated:
-		return redirect('news:latest')
-	else:
-		form = CreateUserForm()
-		if request.method == 'POST':
-			form = CreateUserForm(request.POST)
-			if form.is_valid():
-				form.save()
-				user = form.cleaned_data.get('username')
-				messages.success(request, 'Account was created for ' + user)
+    if request.user.is_authenticated:
+        return redirect('news:latest')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user)
 
-				return redirect('news:login')
-			
+                return redirect('news:login')
 
-		context = {'form':form}
-		return render(request, 'news/register.html', context)
+        context = {'form': form}
+        return render(request, 'news/register.html', context)
 
 
 def loginPage(request):
-	if request.user.is_authenticated:
-		return redirect('news:archive')
-	else:
-		if request.method == 'POST':
-			username = request.POST.get('username')
-			password =request.POST.get('password')
+    if request.user.is_authenticated:
+        return redirect('news:archive')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-			user = authenticate(request, username=username, password=password)
+            user = authenticate(request, username=username, password=password)
 
-			if user is not None:
-				login(request, user)
-				return redirect('news:archive')
-			else:
-				messages.info(request, 'Username OR password is incorrect')
+            if user is not None:
+                login(request, user)
+                return redirect('news:archive')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
 
-		context = {}
-		return render(request, 'news/login.html', context)
+        context = {}
+        return render(request, 'news/login.html', context)
 
 
 def logoutUser(request):
-	logout(request)
-	return redirect('news:login')
+    logout(request)
+    return redirect('news:login')
 
 
 def latest(request):
-    return render(request, 'news/latest.html', 
-    {
-        'bbc_news': bbc_news, 
-        'nbc_news': nbc_news, 
-        'aljazeera': aljazeera,
-        'prothom_alo': prothom_alo,
-        'manob_jomin': manob_jomin,
-        'kaler_kontho': kaler_kontho
-    })
+    return render(request, 'news/latest.html',
+                  {
+                      'bbc_news': bbc_news,
+                      'nbc_news': nbc_news,
+                      'aljazeera': aljazeera,
+                      'prothom_alo': prothom_alo,
+                      'manob_jomin': manob_jomin,
+                      'kaler_kontho': kaler_kontho
+                  })
 
 
 @login_required(login_url='news:login')
 def archive(request):
-    news = News.objects.all()
-    return render(
-        request,
-        'news/archive.html',
-        {'news': news}
-    )
+    if 'q' in request.GET:
+        q = request.GET['q']
+        news = News.objects.filter(headline__icontains=q)
+    else:
+        news = News.objects.all()
+
+    return render(request, 'news/archive.html', {'news': news})
 
 
 @login_required(login_url='news:login')

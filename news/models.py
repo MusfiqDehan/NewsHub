c120336c59356cd923835ff1db9300273055fbc8
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2021-05-16 11:14:58 +0600

    Delete unnecessary fields of model and migrate

--- news/models.py
+++ news/models.py
@@ -1,19 +1,9 @@
 from django.db import models
 from .forms import User
-
+from datetime import datetime
 
 class News(models.Model):
     headline = models.CharField(max_length=1024)
     original_link = models.CharField(max_length=2048)
     img_link = models.CharField(max_length=2048)
     newspaper_name = models.CharField(max_length=255)
-    bookmark = models.ManyToManyField(User, related_name='bookmark', blank=True)
-
-
-class Headline(models.Model):
-    title = models.CharField(max_length=200)
-    image = models.URLField(null=True, blank=True)
-    url = models.TextField()
-    
-    def __str__(self):
-        return self.title

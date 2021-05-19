d228cb702418c63d0a810a5a0da1b678b11a5d83
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2021-05-19 11:42:57 +0600

    Set STATIC_ROOT and run collectstatic for getting static files

--- newshub/settings.py
+++ newshub/settings.py
@@ -124,10 +124,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
-# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'

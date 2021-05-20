7d58cc2a6fc1d9dcc7ae97dc97cfc4df25a02b46
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2021-05-20 12:24:09 +0600

    Add bookmarks and add_to_bookmark urls

--- news/urls.py
+++ news/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('register/', views.registerPage, name="register"),
 	path('login/', views.loginPage, name="login"),  
 	path('logout/', views.logoutUser, name="logout"),
-    path('bookmark/', views.bookmark, name="bookmark"),
+    path('add_to_bookmark/<int:id>', views.add_to_bookmark, name="add_to_bookmark"),
+    path('bookmarks/', views.bookmark_list, name="bookmark_list"),
 
     path('', views.latest, name="latest"),
     path('archive/', views.archive, name="archive")

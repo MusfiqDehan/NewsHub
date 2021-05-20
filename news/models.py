6bca61b2ece139bdd3244a667a1b53d39b620a36
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2021-05-20 12:25:56 +0600

    Add bookmark field to News model

--- news/models.py
+++ news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from .forms import User
 from datetime import datetime
 
@@ -7,3 +8,7 @@ class News(models.Model):
     original_link = models.CharField(max_length=2048)
     img_link = models.CharField(max_length=2048)
     newspaper_name = models.CharField(max_length=255)
+    bookmark = models.ManyToManyField(User, related_name='bookmark', blank=True)
+
+    def get_absolute_url(self):        
+        return reverse('news:archive')
